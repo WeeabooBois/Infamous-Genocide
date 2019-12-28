cf5d93e9022ac4a26bd9d56410ed250e795507f5
NoHaunt <kubsonr1@gmail.com>
2019-12-28 17:55:30 +0300

    login and register pages

--- dd/test/script1.js
+++ dd/test/script1.js
@@ -1,7 +1,3 @@
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
 
 function myFunction() {
   if (window.pageYOffset >= sticky) {
